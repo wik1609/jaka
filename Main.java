a9a1c7a48a0ee0a7c9a45c81e9eeb01fdcde5c47
Wiktor Krawczak <wiko3034gmail.com>
2024-04-08 17:02:37 +0200

    zmiana

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[]args); {
+    public static void main(String[]args) {
     Scanner input = new Scanner(System.in); 
     System.out.println("Podaj liczbę ");
-    }
+    
+    int number = input.nextInt();
+    int lastDigit = number%10; //liczba%10 zawsze wskaze nam ostatnia cyfre
+    
+    
+    System.out.println("Twoja liczba to " + number);
+    System.out.println("Ostatnia cyfra to " + lastDigit);
+    } //% - reszta z dzielenia
 }
