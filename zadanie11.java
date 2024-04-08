7eb91a9917187551637c3b0da7b2a1c218867030
Wiktor Krawczak <wiko3034gmail.com>
2024-04-08 19:05:51 +0200

    zadanie11

---
+++ zadanie11.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class zadanie11 {
+    public static void main(String[] args) {
+
+    Random losowanie = new Random(); {
+    System.out.println("Wylosowane liczby " + losowanie.nextInt(100)+1);
+    }
+}
+}
