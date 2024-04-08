bc78a1985b82399c796f55d95f5d9a06b5a44221
Wiktor Krawczak <wiko3034gmail.com>
2024-04-08 17:35:23 +0200

    zad6

---
+++ zadanie6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class zadanie6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //pierwszy skaner
+        System.out.println("Pierwsza liczba to ");
+        int a = input.nextInt();
+
+        Scanner in = new Scanner(System.in); //drugi skaner
+        System.out.println("Druga liczba to ");
+        int b = in.nextInt();
+
+        int suma = a + b;
+        System.out.println("Suma liczb to " + suma);
+    }
+}
